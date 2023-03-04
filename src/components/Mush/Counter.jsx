288d2af87c21077fb405af55f443099cef50e352
Walobwa_D <walobwadan@gmail.com>
2023-03-04 11:45:01 +0300

    Recorrected Mush's assignment

--- src/components/Mush/Counter.jsx
+++ src/components/Mush/Counter.jsx
@@ -1,21 +1,26 @@
 import './Mush.scss';
 import { useState } from 'react';
-let Counter = () => {
-  let [count, setCount] = useState(0);
-  let myCount = () => {
-    setCount(count + 1);
 
-    count < 5
-      ? document.querySelector('.counter-background').classList.add('green')
-      : document.querySelector('.counter-background').classList.add('red');
-  };
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  let addCount = () => {
+    setCount(count + 1);
+  }
+  let decrement =()=>{
+    setCount(count - 1);
+  }
   return (
     <>
       <div className='counter-background'>
         <p>the count is : {count}</p>
-        <button onClick={myCount}>lick me</button>
+        <button className='w-20 h-20 bg-green-400' onClick={addCount}> + </button>
+        <button className='w-20 h-20 bg-red-400' onClick={decrement}> - </button>
+        {count >= 5 || count<=-5 ? <div className='w-20 h-20 bg-yellow-400'></div> : <div className='w-20 h-20 bg-red-400'></div>}
       </div>
     </>
   );
 };
+
+
 export default Counter;
